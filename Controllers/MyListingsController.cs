539104649b89521c9733fdbb6d9f17ff843b880f
Mihajlo Lujic <mlujic2922it@raf.rs>
2026-05-17 18:58:30 +0200

    Add my listings page

---
+++ Controllers/MyListingsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RVAS_BookExchange.Services;
+
+namespace RVAS_BookExchange.Controllers;
+
+[Authorize]
+public class MyListingsController : Controller
+{
+    private readonly BookService _bookService;
+
+    public MyListingsController(BookService bookService)
+    {
+        _bookService = bookService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Index()
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (userId == null)
+        {
+            return RedirectToAction("Login", "Account");
+        }
+
+        var myBooks = await _bookService.GetByOwnerAsync(userId);
+
+        return View(myBooks);
+    }
+}
