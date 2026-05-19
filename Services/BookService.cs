b7a013f831ef7920d3d8766fb541512d83d988e6
Boris <bbatuta10023it@raf.rs>
2026-05-19 04:03:03 +0200

    Dodao site.css, Layout, Login i Register stranice

--- Services/BookService.cs
+++ Services/BookService.cs
@@ -66,7 +66,7 @@ public class BookService
             filter &= filterBuilder.Eq(book => book.City, city);
         }
 
-        var totalCount = await _books.CountDocumentsAsync(filter);
+        ivar totalCount = await _books.CountDocumentsAsync(filter);
 
         var books = await _books
             .Find(filter)
